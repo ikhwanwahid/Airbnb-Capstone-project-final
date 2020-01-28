d8385996c9f397a859dcd999e09868990d4b9231
ikhwanwahid <ikhyvicky92@gmail.com>
2020-01-28 15:19:43 +0800

    Update app.py

--- December_final/app.py
+++ December_final/app.py
@@ -28,12 +28,10 @@ def get_data2():
     return  pd.read_csv('./k_means_visual.csv')
 
 k_means_visual = get_data2()
+k_means_map = pd.concat([k_means_visual, airbnb_coord], axis=1)
+k_means_map_geo = geopandas.GeoDataFrame(k_means_map, geometry=geopandas.points_from_xy(k_means_map.longitude,
+                                                                            k_means_map.latitude))
 
-@st.cache
-def get_data4():
-    return pd.concat([k_means_visual, airbnb_coord], axis=1)
-k_means_map =get_data4()
-k_means_map_geo = geopandas.GeoDataFrame(k_means_map, geometry=geopandas.points_from_xy(k_means_map.longitude,k_means_map.latitude))
 
 @st.cache
 def get_data3():
